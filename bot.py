8132caf231de2ef4930aca4722cf466f01d5dbe6
HenryCapdevilla <henrydaniel2002@hotmail.com>
2023-09-13 14:35:14 -0500

    Cambio automatico del bot

--- bot.py
+++ bot.py
@@ -24,10 +24,10 @@ while True:
             # Confirma los cambios automáticamente con un mensaje
             subprocess.run(["git", "commit", "-am", "Commit automático antes de la actualización"], cwd=local_repo_path, check=True)
             print("Cambios confirmados exitosamente.")
-            
-            # Realiza un empuje (push) automático para enviar los cambios al repositorio remoto
-            subprocess.run(["git", "push"], cwd=local_repo_path, check=True)
-            print("Cambios empujados al repositorio remoto.")
+
+            # Realiza un pull automático para incorporar las actualizaciones más recientes del repositorio remoto
+            subprocess.run(["git", "pull"], cwd=local_repo_path, check=True)
+            print("Actualización local realizada exitosamente.")
 
         # Si hay cambios en la rama principal, realiza una actualización (pull)
         if "Your branch is behind" in status_output:
@@ -36,8 +36,13 @@ while True:
         else:
             print("No se encontraron cambios en la rama principal.")
 
+        # Realiza un empuje (push) automático para enviar los cambios al repositorio remoto
+        subprocess.run(["git", "push"], cwd=local_repo_path, check=True)
+        print("Cambios empujados al repositorio remoto.")
+
     except subprocess.CalledProcessError as e:
         print(f"Error al ejecutar el comando Git: {e}")
 
     # Espera un tiempo antes de volver a verificar actualizaciones (por ejemplo, cada 5 minutos)
-    time.sleep(50)  # Espera 300 segundos (5 minutos)
+    time.sleep(60)  # Espera 300 segundos (5 minutos)
+
